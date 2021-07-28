15ead132e2c4651279c49f16433a1077871bcdbe
basantashubhu <callmebasanta44@gmail.com>
2021-07-28 10:52:29 +0545

    update question

--- app/Http/Controllers/Api/QuestionApiController.ts
+++ app/Http/Controllers/Api/QuestionApiController.ts
@@ -1,44 +1,75 @@
-import { Request, Response } from "express";
-import { Document } from "mongoose";
-import { Category } from "../../../models/Category";
-import { Question } from "../../../models/Question";
-import { Controller } from "../Kernel/Controller";
+import {Request, Response} from 'express';
+import {Document} from 'mongoose';
+import {Category} from '../../../models/Category';
+import {Question} from '../../../models/Question';
+import {Controller} from '../Kernel/Controller';
 
 export class QuestionApiController extends Controller {
-	constructor() {
-		super();
-		this.except("Admin", "getAll");
-	}
+    constructor() {
+        super();
+        this.except('Admin', 'getAll');
+    }
 
-	saveQuestion(request: Request, response: Response) {
-		if (!this.validate(request, response)) return;
-		Category.findOne({ name : request.body.category }, async (err : Error, result : Document|null) => {
-			if(!err && result === null) {
-				const category = new Category({name : request.body.category})
-				await category.save()
-			}
-		})
-		const question = new Question({
-			label: request.body.label,
-			category: request.body.category,
-			options: request.body.options,
-			difficulty: request.body.difficulty,
-		});
-		question
-			.save()
-			.then((q: Document) => response.status(201).json(q))
-			.catch((err: Error) =>
-				response.status(500).json({ message: err.message })
-			);
-	}
+    saveQuestion(request: Request, response: Response) {
+        if (!this.validate(request, response)) return;
+        Category.findOne({name: request.body.category}, async (err: Error, result: Document | null) => {
+            if (!err && result === null) {
+                const category = new Category({name: request.body.category})
+                await category.save()
+            }
+        })
+        const question = new Question({
+            label: request.body.label,
+            category: request.body.category,
+            options: request.body.options,
+            difficulty: request.body.difficulty,
+        });
+        question
+            .save()
+            .then((q: Document) => response.status(201).json(q))
+            .catch((err: Error) =>
+                response.status(500).json({message: err.message}),
+            );
+    }
 
-	getAll(request: Request, response: Response) {
-		Question.find({}, (err: Error, results: Document[]) => {
-			if (err) return response.status(500).json({ message: err.message });
-			response.json({
-				meta: {},
-				data: results,
-			});
-		});
-	}
+    getAll(request: Request, response: Response) {
+        Question.find({}, (err: Error, results: Document[]) => {
+            if (err) return response.status(500).json({message: err.message});
+            response.json({
+                meta: {},
+                data: results,
+            });
+        });
+    }
+
+    async findOne(request: Request, response: Response) {
+        const question = await Question.findById(request.params.id)
+        response.json(question)
+    }
+
+    /**
+     * update question db level
+     * @param request
+     * @param response
+     */
+    async updateQuestion(request: Request, response: Response) {
+        const question = await Question.findById(request.params.id)
+        if (!question) return response.status(500).json({message: 'Question not found'})
+
+        if (!this.validate(request, response)) return;
+
+        question.label = request.body.label
+        question.category = request.body.category
+        question.difficulty = request.body.difficulty
+
+        if (question.options) {
+            for (let i = 0; i < question.options.length; i++) {
+                const option = question.options[i]
+                option.value = request.body.options[i].value
+                option.is_correct = request.body.options[i].is_correct
+            }
+        }
+        await question.save()
+        response.json(question)
+    }
 }
--- routes/api/route_question.js
+++ routes/api/route_question.js
@@ -17,4 +17,18 @@ router.post('/question', [
 
 router.get('/questions', Kernel.map('QuestionApiController@getAll'))
 
+router.get('/question/:id', Kernel.map('QuestionApiController@findOne'))
+
+router.post('/question/:id/update', [
+    body('label', 'Required').exists({ checkFalsy }),
+    body('category', 'Required').exists({ checkFalsy }),
+    body('difficulty', 'Required').exists({ checkFalsy }).isIn(['Easy', 'Medium', 'Hard']),
+    body('options.*.value', 'Required').exists({ checkFalsy }),
+    body('options.*.is_correct', 'Required').exists({ checkNull: true }),
+    body('options', 'Required').exists({ checkFalsy })
+        .custom(v => Array.isArray(v)).withMessage('Options must be an array')
+        .custom(v => Array.isArray(v) && v.length === 4).withMessage('A question must have 4 options')
+        .custom(v => Array.isArray(v) && v.filter(x => x.is_correct).length === 1).withMessage('Options must have 1 correct answer among all')
+], Kernel.map('QuestionApiController@updateQuestion'))
+
 module.exports = router
